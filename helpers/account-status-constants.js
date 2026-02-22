91f839ed0cb8310b5967153f3307cd28972a3eea
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-02-21 20:44:19 -0600

    feat: Implementando helper para comparar estados de cuenta

---
+++ helpers/account-status-constants.js
@@ -0,0 +1,8 @@
+// para ver el estado en que esta la cuenta (activo, inactivo o desabilitado)
+export const ACCOUNT_STATUS = {
+  ACTIVE:   'activo',
+  INACTIVE: 'inactivo',
+  DISABLED: 'deshabilitado',
+};
+
+export const ACCOUNT_STATUS_VALUES = Object.values(ACCOUNT_STATUS);
