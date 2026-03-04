c05226919f0715deb70a8636186510594c74703f
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-03-04 16:01:25 -0600

    feat: agregando controlador y rutas de retiros

---
+++ src/withdrawals/withdrawals.routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { Router } from 'express';
+import { validateJWT } from '../../middlewares/validate-JWT.js';
+import { isAdmin } from '../../middlewares/is.admin.js';
+import { isAccountOwnerWithdrawal } from '../../middlewares/is_account_owner.js';
+import { validateWithdrawalInput } from '../../middlewares/withdrawals-validation.js';
+import {
+  createRetiro,
+  getRetiros,
+  getRetirosByCuenta,
+} from './withdrawals.controller.js';
+
+const router = Router();
+router.post('/', validateJWT, isAccountOwnerWithdrawal, validateWithdrawalInput, createRetiro);
+
+// Listar todos los retiros (solo admin)
+router.get('/', validateJWT, isAdmin, getRetiros);
+
+// Historial de retiros de una cuenta
+router.get('/cuenta/:id_cuenta', validateJWT, getRetirosByCuenta);
+
+export default router;
