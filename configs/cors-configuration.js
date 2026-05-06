ad268524eb35761ee532b634f1af4f3f5dd5c167
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2026-05-05 19:44:54 -0600

    chore: limpieza de la arquitectura monolítica y archivos antiguos

--- configs/cors-configuration.js
+++
@@ -1,9 +0,0 @@
-//desde donde nos pueden hacer peticiones, y cuales
-export const corsOptions = {
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
- 
- 
--- helpers/account-status-constants.js
+++
@@ -1,8 +0,0 @@
-// para ver el estado en que esta la cuenta (activo, inactivo o desabilitado)
-export const ACCOUNT_STATUS = {
-  ACTIVE:   'activo',
-  INACTIVE: 'inactivo',
-  DISABLED: 'deshabilitado',
-};
-
-export const ACCOUNT_STATUS_VALUES = Object.values(ACCOUNT_STATUS);
