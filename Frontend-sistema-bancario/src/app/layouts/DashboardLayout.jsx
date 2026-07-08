c0d2f36311eae482ad695d9aba9bc854704679ce
jmagzul-2024029 <jmagzul-2024029@kinal.edu.gt>
2026-07-07 19:38:46 -0600

    feat: Mejorar la organización del encabezado de página

--- Frontend-sistema-bancario/src/app/layouts/DashboardLayout.jsx
+++ Frontend-sistema-bancario/src/app/layouts/DashboardLayout.jsx
@@ -22,16 +22,16 @@ export const DashboardLayout = () => {
 
       <div className="flex-1 flex flex-col lg:ml-64 overflow-hidden">
         {/* Header */}
-        <header className="h-20 bg-surface border-b border-border flex items-center justify-between px-10 sticky top-0 z-20">
+        <header className="h-20 bg-surface border-b border-border flex items-center justify-between px-4 sm:px-6 lg:px-10 sticky top-0 z-20">
 
-          {/* Botón para abrir el menú del sidebar*/}
-          <button onClick={toggle} className="text-text-primary lg:hidden cursor-pointer">
-            <Menu className="w-6 h-6" />
-          </button>
-
-          <h2 className="text-xl font-bold text-text-primary capitalize">
-            {role === 'ADMIN_ROLE' ? 'Panel de Control' : 'Mi Banca Digital'}
-          </h2>
+          <div className="flex items-center gap-3">
+            <button onClick={toggle} className="text-text-primary lg:hidden cursor-pointer">
+              <Menu className="w-6 h-6" />
+            </button>
+            <h2 className="text-base sm:text-xl font-bold text-text-primary capitalize">
+              {role === 'ADMIN_ROLE' ? 'Panel de Control' : 'Mi Banca Digital'}
+            </h2>
+          </div>
 
           <div className="flex items-center gap-6">
             <CurrencyPicker />
