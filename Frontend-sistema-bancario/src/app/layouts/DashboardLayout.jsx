96d90f80db9d816b556176fca1ada917234a2cf8
jmagzul-2024029 <jmagzul-2024029@kinal.edu.gt>
2026-07-07 19:41:33 -0600

    feat: arreglo de tamaño de texto

--- Frontend-sistema-bancario/src/app/layouts/DashboardLayout.jsx
+++ Frontend-sistema-bancario/src/app/layouts/DashboardLayout.jsx
@@ -53,7 +53,7 @@ export const DashboardLayout = () => {
         </header>
 
         {/* Contenido */}
-        <main className="flex-1 p-10 overflow-y-auto">
+        <main className="flex-1 p-4 sm:p-6 lg:p-10 overflow-y-auto">
           <motion.div
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
